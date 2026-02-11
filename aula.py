29b1587bef9cad1478be76748b0dd6749e38883d
NeroDK01 <nickname0235@gmail.com>
2026-02-11 12:40:05 -0300

    aula

---
+++ aula.py
@@ -0,0 +1,75 @@
+# Operadores Aritimeticos
+# Entrada e escrita de dados
+# Estrutura de decisao e logica condicional
+
+# Entrada e Escrita
+
+# nome = input("Qual é o seu nome?")
+# idade = input("Qual a sua idade?")
+# profissao = input("Qual sua profissao?")
+# salario = input("Qual sua media salarial?")
+
+# print("O seu nome é: ", nome, "\n A sua idade é de:", idade, "\n A sua profissao é:", profissao, "\n Seu salario é:", salario)
+
+#fazer um programa que perguntara o nome do usuario
+#a profissao e quanto ele ganah e mostrar a ele os dados digitados
+
+ # Operadores Aritimeticas
+
+# soma = 1 + 2
+# multiplicacao = 2 * 2
+# subtracao = 2 - 2
+# divisao = 2 / 2
+# operacao = input("Digite a operação (+, -, *, /): ")
+# .int é um valor para numeros inteiro = que nao possuem cassas decimais
+# float é para numeros decimais
+# float aceita numeros quebrados
+
+# cast
+
+
+
+# valor1 = int(input("digite o valor 1: "))
+# valor2 = int(input("digite o valor 2: "))
+
+# print = ("A soma dos valores é de: ", soma)
+
+
+
+# soma = +,  multiplicacao = *, divisao = /, subtraçao = -
+
+# cast para float 
+
+# valor1 = float(input("digite o valor 1 : "))
+
+# soma = 1.5 + valor1
+
+# print( "soma é de: ", soma)
+
+# faça uma calculadora para o peso do usuaro em kg
+# e a altura em metros e calcule o imc
+# a formula do imc e o peso dividido pela altura ao quadrad
+
+# peso = float(input("digite o seu peso em kg : "))
+# altura = float(input("digite sua( altura em metros : "))
+
+# imc = peso / (altura * altura))
+
+# print("o imc e: ", imc)
+
+a = float(input( "digite o valor de a :"))
+b = float(input("digite o valor de b :" ))
+c = float(input( "digite o valor de c :"))
+
+# o pyton suporta a equação de elevação nativamente ex: b**2
+
+delta = (b**2) -  (4 * a * c)
+
+x1 = (-b + (delta**0.5)) / 2 * a
+x2 = (-b - (delta**0.5)) / 2 * a
+print ( "x1= ", x1, "x2= ", x2)
+
+
+
+
+
